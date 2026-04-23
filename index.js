f08fa9fe7f4bdad7d11f6a0a10c2cefdfb778bec
vitto2 <vitor2071@gmail.com>
2026-04-23 20:24:44 -0300

    feat: faz conexão com mongoDB

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-2
+const dotenv = require("dotenv");
+dotenv.config();
+
+const express = require("express");
+const connectToDatabase = require("./src/database/mongoose.database");
+
+const app = express();
+
+connectToDatabase();
+
+app.get("/", (req, res) => {
+    res.status(200).send("toma aí tua resposta, otaro!");
+});
+
+app.listen(8000, () => {
+    console.log("Listening on a port 8000!");
+});
