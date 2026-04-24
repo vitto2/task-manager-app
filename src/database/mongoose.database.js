ee712601c87c28f001ca2befa2940b566c5d0178
vitto2 <vitor2071@gmail.com>
2026-04-24 17:37:41 -0300

    fix: corrige método de conexão com banco de dados

--- src/database/mongoose.database.js
+++ src/database/mongoose.database.js
@@ -5,7 +5,7 @@ const connectToDatabase = async () => {
         // `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.pyhwodh.mongodb.net/?appName=Cluster0`
         `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@ac-paa9jen-shard-00-00.pyhwodh.mongodb.net:27017,ac-paa9jen-shard-00-01.pyhwodh.mongodb.net:27017,ac-paa9jen-shard-00-02.pyhwodh.mongodb.net:27017/?ssl=true&replicaSet=atlas-dm9vb9-shard-0&authSource=admin&appName=Cluster0`
     );
-    console.log("Conectado ao mongoDB caralho!!!");
+    console.log("Conectado ao mongoDB!!!");
 };
 
 module.exports = connectToDatabase;
